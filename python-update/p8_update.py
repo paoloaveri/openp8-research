b527a35d61402b20c8ee1488bba60c9b961836f8
Paolo Averi <contact.paolo96@gmail.com>
2020-06-15 23:54:37 -0400

    Implemented --log

--- python-update/p8_update.py
+++ python-update/p8_update.py
@@ -27,6 +27,16 @@ gb_update_file_len = 0
 gb_simulation_send_next = False
 gb_disable_disconnect_handler = False
 
+def debug_log(s):
+    global gb_arg_debug
+    global gb_arg_log
+    global gb_log_file_handle
+    if gb_arg_debug:
+        print(s)
+    if gb_arg_log:
+        gb_log_file_handle.write(str(s)+'\n')
+        gb_log_file_handle.flush()
+
 # Print iterations progress from https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '#', printEnd = "\r"):
     """
@@ -78,7 +88,6 @@ def p8_crc16(data: bytearray):
     return crc16_ccitt(0xFEEA, data)
 
 async def send_command(cmd: int, data: bytearray):
-    global gb_arg_debug
     global gb_bleak_client
     data2send = bytearray()
     data2send.append(0xFE)
@@ -87,8 +96,7 @@ async def send_command(cmd: int, data: bytearray):
     data2send.append(len(data)+5)
     data2send.append(cmd)
     data2send.extend(data)
-    if gb_arg_debug:
-        print(''.join('0x{:02x},'.format(x) for x in data2send))
+    debug_log(''.join('0x{:02x},'.format(x) for x in data2send))
     await gb_bleak_client.write_gatt_char("fee2", data2send)
 
 async def read_gatt(uuid: str):
@@ -104,8 +112,7 @@ async def reconnect():
     print("Device disconnected. Waiting 5 seconds to see if it automatically reconnects...")
     await asyncio.sleep(5)
     is_connected = await gb_bleak_client.is_connected()
-    if gb_arg_debug:
-        print(f"Device connected?: {is_connected}")
+    debug_log(f"Device connected?: {is_connected}")
     if is_connected:
         print("We're good, device reconnected automatically")
     else:
@@ -134,7 +141,6 @@ def dummy_disconnected_handler(client):
 def notification_handler_fee3(sender, data: bytes):
     debug = True
     #"import" global variables
-    global gb_arg_debug
     global gb_fee3_complete_len
     global gb_fee3_complete_cmd
     global gb_fee3_received_len
@@ -146,8 +152,7 @@ def notification_handler_fee3(sender, data: bytes):
     global gb_state_update_finished
 
     gb_fee3_notify_counter += 1
-    if gb_arg_debug:
-        print(f"Notify on 0xffe3 number #{gb_fee3_notify_counter}")
+    debug_log(f"Notify on 0xffe3 number #{gb_fee3_notify_counter}")
 
     ba = bytearray(data)
     # print(ba)
@@ -168,9 +173,8 @@ def notification_handler_fee3(sender, data: bytes):
         print("waiting to receive next part")
         return
 
-    if gb_arg_debug:
-        print("received full command: ")
-        print(''.join('0x{:02x},'.format(x) for x in gb_fee3_complete_cmd))
+    debug_log("received full command: ")
+    debug_log(''.join('0x{:02x},'.format(x) for x in gb_fee3_complete_cmd))
     
     if (ba == bytearray([0xFE, 0xEA, 0x10, 0x07, 0x63, 0x0, 0x0])) and not(gb_state_watch_in_DFU_MODE):
         # update haven't stated yet (block number is 0)
@@ -180,8 +184,7 @@ def notification_handler_fee3(sender, data: bytes):
     elif (ba[:5] == bytearray([0xFE, 0xEA, 0x10, 0x07, 0x63])) and gb_state_watch_in_DFU_MODE:
         # send block according to received number
         block_256_bytes_nb = int.from_bytes(ba[5:7], byteorder='big', signed=False)
-        if gb_arg_debug:
-            print(f"Received request for block nb: {block_256_bytes_nb}")
+        debug_log(f"Received request for block nb: {block_256_bytes_nb}")
         # print("Sending only first block this time, stopping now...")
         # return
         asyncio.create_task(send_block(block_256_bytes_nb))
@@ -202,7 +205,6 @@ def notification_handler_fee3(sender, data: bytes):
         print("continuing aborted update...")
 
 async def send_block(block_nb: int):
-    global gb_arg_debug
     global gb_current_block_nb
     global gb_update_file_data
     global gb_update_file_len
@@ -218,24 +220,21 @@ async def send_block(block_nb: int):
         # you can do "tail -f current.txt" in another terminal to monitor progress without being flooded with debug messages
         with open("current.txt", "w") as f:
             f.write(f"Sending update block nb: {block_nb}/{int(gb_update_file_len/256)}"+'\n')
-        print(''.join('0x{:02x},'.format(x) for x in current_block))
+        debug_log(''.join('0x{:02x},'.format(x) for x in current_block))
     else:
         printProgressBar(block_nb, int(gb_update_file_len/256), suffix = f"(Sending update block nb: {block_nb}/{int(gb_update_file_len/256)})", length = 40)
     # create packet to be sent:
     packet = bytearray([0xFE]) # start byte
     current_block_crc16 = p8_crc16(current_block) # block nb
-    if gb_arg_debug: 
-        print(f"Calculated CRC16 for the block is 0x{current_block_crc16:04x}")
+    debug_log(f"Calculated CRC16 for the block is 0x{current_block_crc16:04x}")
     packet.extend(bytearray(current_block_crc16.to_bytes(2, byteorder = 'big'))) # 2 bytes for crc16 of the block
     packet.extend(bytearray((len(current_block) & 0xFF).to_bytes(1, byteorder = 'big'))) # 1 byte for length of the block
     packet.extend(current_block) # actual data
-    if gb_arg_debug:
-        print("Full packet is:")
-        print(''.join('0x{:02x},'.format(x) for x in packet))
+    debug_log("Full packet is:")
+    debug_log(''.join('0x{:02x},'.format(x) for x in packet))
     await send_packet(packet)
 
 async def send_packet(data: bytearray):
-    global gb_arg_debug
     global gb_arg_simulation
     global gb_bleak_client
     global gb_simulation_send_next
@@ -244,9 +243,8 @@ async def send_packet(data: bytearray):
         part_nb = 0
         while (len(data) - part_nb * 20) > 20:
             packet_part = data[part_nb*20:part_nb*20+20]
-            if gb_arg_debug:
-                print(f"Packet part {part_nb} is:")
-                print(''.join('0x{:02x},'.format(x) for x in packet_part))
+            debug_log(f"Packet part {part_nb} is:")
+            debug_log(''.join('0x{:02x},'.format(x) for x in packet_part))
             if not(gb_arg_simulation):
                 await gb_bleak_client.write_gatt_char("fee5", packet_part)
             part_nb += 1
@@ -258,15 +256,13 @@ async def send_packet(data: bytearray):
             await asyncio.sleep(0.001)
         if (len(data) - part_nb * 20) > 0:
             packet_part = data[part_nb*20:]
-            if gb_arg_debug:
-                print(f"Last packet part {part_nb} is:")
-                print(''.join('0x{:02x},'.format(x) for x in packet_part))
+            debug_log(f"Last packet part {part_nb} is:")
+            debug_log(''.join('0x{:02x},'.format(x) for x in packet_part))
             if not(gb_arg_simulation):
                 await gb_bleak_client.write_gatt_char("fee5", packet_part)
     else:
-        if gb_arg_debug:
-            print("Packet part is:")
-            print(''.join('0x{:02x},'.format(x) for x in data))
+        debug_log("Packet part is:")
+        debug_log(''.join('0x{:02x},'.format(x) for x in data))
         if not(gb_arg_simulation):
             await gb_bleak_client.write_gatt_char("fee5", data)
         print("small packet")
@@ -294,7 +290,7 @@ async def test_update_simulated():
         gb_update_file_data = bytearray(f.read())
         gb_update_file_len = len(gb_update_file_data)
         print(f"{gb_arg_filename} size: {gb_update_file_len}")
-        print(''.join('0x{:02x},'.format(x) for x in gb_update_file_len.to_bytes(4, byteorder = 'big')))
+        debug_log(''.join('0x{:02x},'.format(x) for x in gb_update_file_len.to_bytes(4, byteorder = 'big')))
         
     crc = p8_crc16(gb_update_file_data)
     print(f"Calculated CRC16 is {crc} or 0x{crc:04x} in hex")
@@ -327,23 +323,19 @@ async def run():
     services = ["FEEA"]
     devices = await get_connected_by_services(service_uuids=services)
     for d in devices:
-        if gb_arg_debug:
-            print(d)
+        debug_log(d)
         if(d.name == "P8 a" or d.name == "P8a"):
             print("P8 is already connected, great!!")
-            if gb_arg_debug:
-                print(d.__dict__)
+            debug_log(d.__dict__)
             gb_p8_address = d.address
     # If not already connect, scan for it
     if not gb_p8_address:
         devices = await discover()
         for d in devices:
-            if gb_arg_debug:
-                print(d)
+            debug_log(d)
             if(d.name == "P8 a" or d.name == "P8a"):
                 print("P8 watch was found!!")
-                if gb_arg_debug:
-                    print(d.__dict__)
+                debug_log(d.__dict__)
                 gb_p8_address = d.address
 
         if(gb_p8_address == None):
@@ -354,24 +346,21 @@ async def run():
         gb_bleak_client = client
         # gb_bleak_client.set_disconnected_callback(disconnected_handler)
         is_connected = await client.is_connected()
-        if gb_arg_debug:
-            print(f"Device connected?: {is_connected}")
+        debug_log(f"Device connected?: {is_connected}")
 
         if not(gb_update_file_crc16): # don't calculate again if already did before
             with open(gb_arg_filename, 'rb') as f:
                 gb_update_file_data = bytearray(f.read())
                 gb_update_file_len = len(gb_update_file_data)
                 print(f"{gb_arg_filename} size: {gb_update_file_len}")
-                if gb_arg_debug:
-                    print(''.join('0x{:02x},'.format(x) for x in gb_update_file_len.to_bytes(4, byteorder = 'big')))
+                debug_log(''.join('0x{:02x},'.format(x) for x in gb_update_file_len.to_bytes(4, byteorder = 'big')))
 
             crc = p8_crc16(gb_update_file_data)
             gb_update_file_crc16 = crc
             print(f"Calculated CRC16 is 0x{crc:04x}")
         
         gatt = await client.read_gatt_char("2a24")
-        if gb_arg_debug:
-            print(gatt)
+        debug_log(gatt)
         # while await client.is_connected():
         #     await asyncio.sleep(0.5)
         # return
@@ -387,9 +376,7 @@ async def run():
 
         while await client.is_connected():
             await asyncio.sleep(0.5)
-        # print("On Mac, go to your System Preferences, and manually remove P8 a.")
-        # input("Press Enter to continue...")
-        # return
+
     if(gatt == bytearray(b'DFU=0')):
         print("Wait 7 seconds for the watch to reboot")
         await asyncio.sleep(7) #wait for the watch to reboot
@@ -400,14 +387,12 @@ async def run():
         gb_bleak_client = client
         # gb_bleak_client.set_disconnected_callback(disconnected_handler)
         is_connected = await client.is_connected()
-        if gb_arg_debug:
-            print(f"Device connected?: {is_connected}")
+        debug_log(f"Device connected?: {is_connected}")
 
         await client.start_notify("fee3", callback=notification_handler_fee3)
 
         gatt = await client.read_gatt_char("2a24")
-        if gb_arg_debug:
-            print(gatt)
+        debug_log(gatt)
         if(gatt == bytearray(b'DFU=1')):
             print("DFU=1. We can continue!")
         else:
@@ -430,6 +415,7 @@ def main():
     global gb_arg_log
     global gb_arg_simulation
     global gb_state_update_finished
+    global gb_log_file_handle
     parser = argparse.ArgumentParser(description='Update P8 watch over BLE')
     parser.add_argument('file', help='update file to be uploaded at 0x23000')
     parser.add_argument('--debug', '-d', action='store_true', help='print debug log')
@@ -439,12 +425,14 @@ def main():
 
     args = parser.parse_args()
     gb_arg_filename = args.file
-    gb_arg_debug =args.debug
-    gb_arg_log =args.log
-    gb_arg_simulation =args.simulation
+    gb_arg_debug = args.debug
+    gb_arg_log = args.log
+    gb_arg_simulation = args.simulation
 
     if gb_arg_debug:
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    if not(gb_arg_log is None):
+        gb_log_file_handle = open(gb_arg_log, "w+")
     loop = asyncio.get_event_loop()
     if gb_arg_simulation:
         loop.run_until_complete(test_update_simulated())
